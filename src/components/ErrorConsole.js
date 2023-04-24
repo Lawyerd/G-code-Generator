3128f725f1fb7e785a3626e40be26f37cb302080
MASTER <jun12376@gmail.com>
2023-04-24 15:32:48 +0900

    Add media-handling

--- src/components/ErrorConsole.js
+++ src/components/ErrorConsole.js
@@ -3,7 +3,10 @@ import "../css/ErrorConsole.css"
 function ErrorConsole({ error }) {
     return (
         <pre className="error-console">
+            <div className="error-message">
             {error}
+            </div>
+            
         </pre>
     );
 }
